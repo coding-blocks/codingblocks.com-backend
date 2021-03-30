0030f1f950b25bd37ff69f9ea410878dde3505a6
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-03-30 16:28:35 +0530

    use timestamp instead of string literal

--- cbdb/settings.py
+++ cbdb/settings.py
@@ -172,6 +172,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DATE_FORMAT': '%s',
 
 }
