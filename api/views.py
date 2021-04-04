2907da747489fbe02253e7d7cd310a2784c06132
BipinKalra <bipinkalra@gmail.com>
2021-04-04 14:10:48 +0530

    events model complete

--- api/views.py
+++ api/views.py
@@ -60,8 +60,15 @@ class CourseRetrieveView(generics.RetrieveAPIView):
     lookup_field = 'slug'
 
 
+class EventsList(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventsSerializer
+    filterset_fields = ['title']
 
-
+# class EventRetrieveView(generics.RetrieveAPIView):
+#     queryset = Event.objects.prefetch_related(Prefetch('batch_set')).all()
+#     serializer_class = EventSerializer
+#     lookup_field = 'slug'
 
 
 
